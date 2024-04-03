a74fc16f4fc586676a08cf22907c7279365bdd51
QuanTrieuPCYT <admin@qtpc.tech>
2024-04-03 15:00:12 +0700

    fixed undefined crashes

--- plugins/gtfo/src/index.ts
+++ plugins/gtfo/src/index.ts
@@ -40,16 +40,60 @@ const Main = {
                         let findReactions = buttons.find(b => b?.props.message?.toLowerCase() == 'reactions');
                         // let findJumpToReference = buttons.find(b => b?.props?.message?.toLowerCase() == 'jump to reference');
                         // let findCreateThread = buttons.find(b => b?.props?.message?.toLowerCase() == 'create thread');
+                       
+                        if (findReport) {
+                            const index = buttons.indexOf(findReport);
+                            if (index !== -1) buttons.splice(index, 1);
+                        }
 
-                        if (findReport) delete buttons[buttons.indexOf(findReport)]
-                        if (findMarkUnread) delete buttons[buttons.indexOf(findMarkUnread)]
+                        if (findReactions) {
+                            const index = buttons.indexOf(findReactions);
+                            if (index !== -1) buttons.splice(index, 1);
+                        }
+
+                        /* // this shit seems to crash somehow. i'll just comment it here as i need it sometimes as well
+                        if (findMarkUnread) {
+                            const index = buttons.indexOf(findMarkUnread);
+                            if (index !== -1) buttons.splice(index, 1);
+                        }
+                        /*
+
+                        if (findCopyMessageId) { // i know this can be toggled by toggling developer mode, but i don't fucking care.
+                            const index = buttons.indexOf(findCopyMessageId);
+                            if (index !== -1) buttons.splice(index, 1);
+                        }
+
+
+                        if (findMention) {
+                            const index = buttons.indexOf(findMention);
+                            if (index !== -1) buttons.splice(index, 1);
+                        }
+
+
+                        if (findMessageButton) {
+                            const index = buttons.indexOf(findMessageButton);
+                            if (index !== -1) buttons.splice(index, 1);
+                        }
+
+                        /*
+                        if (findJumpToReference) {
+                            const index = buttons.indexOf(findJumpToReference);
+                            if (index !== -1) buttons.splice(index, 1);
+                        }
+                        */ // this fuck shit
+
+                        /* // old button removing behavior
+                        if (findReport?) delete buttons[buttons.indexOf(findReport)]
+                        if (findMarkUnread?) delete buttons[buttons.indexOf(findMarkUnread)]
                         if (findMention) delete buttons[buttons.indexOf(findMention)]
                         if (findCopyMessageId) delete buttons[buttons.indexOf(findCopyMessageId)]
                         if (findMessageButton) delete buttons[buttons.indexOf(findMessageButton)]
-                        // if (findJumpToReference) delete buttons[buttons.indexOf(findJumpToReference)]
-                        // if (findCreateThread) delete buttons[buttons.indexOf(findCreateThread)]
-                        if (findReactions) delete buttons[buttons.indexOf(findReactions)]
-                    }
+                        if (findJumpToReference) delete buttons[buttons.indexOf(findJumpToReference)]
+                        if (findCreateThread) delete buttons[buttons.indexOf(findCreateThread)]
+                        if (findReactions?) delete buttons[buttons.indexOf(findReactions)]
+                        */
+
+                  }
                 )})
             })
     )},
